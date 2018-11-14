374c0f6ea7220aa99356e4e8d131ec2bb5546b4d
Beata Karolczak <karolczak.beata@gmail.com>
2018-11-14 17:42:53 +0100

    Add ng-switch directive

--- directives/js/MoviesController.js
+++ directives/js/MoviesController.js
@@ -2,19 +2,23 @@ function MoviesController() {
   this.favorites = [{
     title: "Shawshank",
     year: "1994",
-    popular: true
+    popular: true,
+    category: 'thriller'
   },{
     title: "Inception",
     year: "2010",
-    popular: false
+    popular: false,
+    category: 'fantasy'
   },{
     title: "The Matrix",
     year: "1999",
-    popular: true
+    popular: true,
+    category: 'fantasy'
   },{
     title: "Saving Private",
     year: "1998",
-    popular: true
+    popular: true,
+    category: 'action'
   }];
 }
 
