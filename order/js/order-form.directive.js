36315d21e0861a1fde53ad7648bfc9840605986d
Beata Karolczak <karolczak.beata@gmail.com>
2018-11-18 23:31:06 +0100

    Add ng-options - forms

--- order/js/order-form.directive.js
+++ order/js/order-form.directive.js
@@ -8,14 +8,18 @@ function orderForm() {
     },
     controller: 'OrderFormController as form',
     template: `
-      <pre>{{ form.data | json }}</pre>
       <pre>{{ orderForm | json }}</pre>
-      <pre>{{ orderForm.name | json }}</pre>
+      <pre>{{ orderForm.orderChoice | json }}</pre>
+      <pre>{{ form.data | json }}</pre>
       <form name="orderForm" novalidate ng-submit="form.onSubmit();">
         <input name="name" required="" type="text" ng-model="form.data.name" placeholder="Your name">
         <input name="email" required="" type="email" ng-model="form.data.email" placeholder="Your email">
         <input name="location" required="" type="text" ng-model="form.data.location" placeholder="Your location">
-        <select>
+        <select
+          name="orderChoice"
+          required=""
+          ng-model="form.data.product"
+          ng-options="product.label for product in form.products">
           <option value="">Select...</option>
         </select>
         <textarea name="comments" required="" ng-model="form.data.comments" placeholder="Any messages (optional)"></textarea>
